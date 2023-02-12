68942343a8097a67eb74da646a66675f78f48a0a
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-02-12 13:55:09 -0300

    Update ExcelSheetColumnTitle.py

--- ExcelSheetColumnTitle.py
+++ ExcelSheetColumnTitle.py
@@ -1,4 +1,6 @@
 #https://leetcode.com/problems/excel-sheet-column-title/description/
+
+#It is like in a number system in which after 9 it is 00 instead of 10; or like a system without zero: starting with 1, 2.. and after 9 it is 11.
 def convertToTitle(c):
     
     al =[chr(i) for i in range(ord('A'),ord('Z')+1)]
